a932ae9cd47ea056a398fce95601ebf0b450fc3a
root <root@arm.localdomain>
2016-02-19 07:51:01 +0000

    test

--- src/accessor.cc
+++ src/accessor.cc
@@ -28,6 +28,63 @@ static int spi_fd = -1 ;
 
 using namespace v8;
 
+/*
+** Function : spi_open
+** Note     : open spidev1.0 node for transfer
+** return   : success return open fd else return -1
+*/
+int
+spi_open()
+{
+	if(access(SPIDEV,EXISTMODE) < 0) return -1 ;
+	if((spi_fd = open(SPIDEV,MODE)) < 0) return -1 ;
+
+	return spi_fd ;
+}
+
+/*
+** Function : spi_close
+** Note     : close spidev1.0 opened fd
+** return   : success return 0 else return -1
+*/
+int
+spi_close()
+{
+	if(spi_fd == -1) return -1 ;
+	close(spi_fd) ;
+	return 0 ;
+}
+
+
+/*
+** Function   : spi_read
+** Note       : read message frome spidev
+** return     : success return read size else return -1
+*/
+
+int
+spi_read(void* buf, int size)
+{
+	if((spi_fd == -1) || (buf == NULL) || (size <= 0)) return -1 ;
+	int size_read = read(spi_fd,buf,size) ;
+	if(size_read < 0) return -1 ;
+	return size_read ;
+}
+
+/*
+** Function   :spi_write
+** Note       :write message to spidev
+** return     :success return write size else return -1
+*/
+int
+spi_write(void* buf ,int size)
+{
+	if((spi_fd == -1) || (NULL == buf) || (size <= 0)) return -1 ;
+	int size_write = write(spi_fd,buf,size) ;
+	if(size_write < 0) return -1 ;
+	return size_write ;
+}
+
 void RunCallback(const FunctionCallbackInfo<Value>& args) {
         Isolate* isolate=Isolate::GetCurrent();
         HandleScope scope(isolate);
@@ -90,7 +147,7 @@ void RunCallback(const FunctionCallbackInfo<Value>& args) {
 
         //bcm2835_spi_end();
         //bcm2835_close();
-        close(fd);
+        spi_close(fd);
 }
 
 void Init(Handle<Object> exports, Handle<Object> module) {
@@ -100,63 +157,6 @@ void Init(Handle<Object> exports, Handle<Object> module) {
 
 uint8_t initRfidReader(void) {
     return spi_open();
-}
-
-/*
-** Function : spi_open
-** Note     : open spidev1.0 node for transfer
-** return   : success return open fd else return -1
-*/
-int
-spi_open()
-{
-	if(access(SPIDEV,EXISTMODE) < 0) return -1 ;
-	if((spi_fd = open(SPIDEV,MODE)) < 0) return -1 ;
-
-	return spi_fd ;
-}
-
-/*
-** Function : spi_close
-** Note     : close spidev1.0 opened fd
-** return   : success return 0 else return -1
-*/
-int
-spi_close()
-{
-	if(spi_fd == -1) return -1 ;
-	close(spi_fd) ;
-	return 0 ;
-}
-
-
-/*
-** Function   : spi_read
-** Note       : read message frome spidev
-** return     : success return read size else return -1
-*/
-
-int
-spi_read(void* buf, int size)
-{
-	if((spi_fd == -1) || (buf == NULL) || (size <= 0)) return -1 ;
-	int size_read = read(spi_fd,buf,size) ;
-	if(size_read < 0) return -1 ;
-	return size_read ;
-}
-
-/*
-** Function   :spi_write
-** Note       :write message to spidev
-** return     :success return write size else return -1
-*/
-int
-spi_write(void* buf ,int size)
-{
-	if((spi_fd == -1) || (NULL == buf) || (size <= 0)) return -1 ;
-	int size_write = write(spi_fd,buf,size) ;
-	if(size_write < 0) return -1 ;
-	return size_write ;
 }
 
 NODE_MODULE(rc522, Init)
